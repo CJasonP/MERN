9b55a51fc66ebb58aad8b0ff1e8a1b0ce533db8d
CJasonP <jasonpurnomo12@gmail.com>
2026-05-10 21:48:42 +0700

    revised for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv";
 import cors from "cors"
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
@@ -12,11 +13,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve()
 
-
-app.use(cors({
-    origin: "http://localhost:5173",
-}))
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }))
+}
 app.use(express.json()) //this middleware pass json body
 app.use(rateLimiter)
 // app.use((req, res, next) => {
@@ -28,6 +31,14 @@ app.use(rateLimiter)
 
 app.use("/api/notes", notesRoutes);
 // app.use("/api/product", productRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 
 connectDB().then(()=>{
